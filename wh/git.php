a306fb44f72496dfaca5dda32ed2589f1bc6bc6e
zeroibc@qq.com <fujunjie970722>
2018-07-10 12:00:59 +0800

    .

--- wh/git.php
+++ wh/git.php
@@ -4,8 +4,6 @@ include './webhook.class.php';
 set_time_limit(0);
 date_default_timezone_set('Asia/Shanghai');
 
-echo 123;
-
 $config = array(
     'token_field' => 'X-Hub-Signature',
     'access_token' => '850c4c27dcb252b81e4b783f83a8a19be44db6ee',
